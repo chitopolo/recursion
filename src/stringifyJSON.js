22a8ac6ec275d2905be645f916378b01f03e8d04
Rene Polo <renejpolo@me.com>
2014-10-06 20:11:49 -0400

    "empezando con fuerza..."

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,20 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  var type = typeof(obj);
+  
+  if(type === 'number'|| obj === null || type === 'boolean'){
+  console.log('valor: '+ obj + ' typo: ' + type + ' Valor final: '+ String(""+obj+""));
+  	var result = String(''+obj+''); 
+
+  	return result;
+  }else if(type === "string"){
+  	var result = String('"'+obj+'"');
+  	return result; 
+  }
+  else if(type === 'object'){
+  	var result = String(''+obj+''); 
+  	console.log('valor: '+ obj + ' typo: ' + type + ' Valor final: '+ String(""+obj+""));
+  	return result;
+  }
 };
