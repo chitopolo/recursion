1868d580b94b10d47be35fcb4697702e345d0720
Rene Polo <renejpolo@me.com>
2014-10-07 11:39:04 -0400

    "finished stringifyJSON"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,55 +5,37 @@
 var stringifyJSON = function(obj) {
   // your code goes here
   var type = typeof(obj);
-  console.log('tipo: '+type );
   var result = '';
-
-  if(obj === null){
-    console.log('null');
-     result = ''+obj+'';
-  }
-  else if(type === 'boolean'){
-    console.log('boolean');
-     result = ''+obj+'';
-  }
+  if(obj === null || type === 'boolean' || type ==='number') result = ''+obj+'';
   else if(type === 'string'){
     result = '"'+obj+'"';
   }
-
-  else if(type ==='number'){
-    console.log('Number');
-    result = ''+obj+'';
-  }
   else if(Array.isArray(obj)){
-      console.log('length: '+obj.length);
+    console.log('length: '+obj.length);
     if(obj.length<1){
       result = '[]';
     }else{
-       var items = [];
+      var items = [];
       for(var num in obj){
         items.push(stringifyJSON(obj[num]));
       }
-       result = '['+ items + ']';
+      result = '['+ items + ']';
     }
-
   }else{
     var items = [];
-
-      for (var subKey in obj){
-          
-          
-            items += '"' + subKey + '":';
-            stringifyJSON(obj[subKey]);
-            items += ',';
-          
-        }
-
-
-    
-       result = '{'+ items + '}';
+    var count = 0;
+    for (var subKey in obj){
+      if(obj.hasOwnProperty(subKey)) ++count ;
+      if(isStringifiable(obj[subKey])){
+        if(count>1) items += ',' ;
+        items += '"' + subKey + '":';
+        items += stringifyJSON(obj[subKey]);
+      }
+    }
+    result = '{'+ items + '}';
   }
-
-
+  function isStringifiable(value) {
+    return typeof value !== 'undefined' && typeof value !== 'function';
+  }
   return result;
-
 };
